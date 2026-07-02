45cbdb63efcc81db96c9432cad414f3b640c5fbb
anandasilvia2006 <anandasilvia2006@gmail.com>
2026-07-02 10:12:00 +0700

    Membuat database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,17 +3,32 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'],
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+            ]
+        );
 
-        $this->call([
-
-            AdminSeeder::class,
-
-        ]);
-
+        User::updateOrCreate(
+            ['email' => 'customer@gmail.com'],
+            [
+                'name' => 'Customer',
+                'password' => Hash::make('customer123'),
+                'role' => 'customer',
+            ]
+        );
     }
 }
